3c6d46d689a56900b82de0cebcc6bd46be1f4ce1
ortiz <victordortizg@gmail.com>
2014-03-10 14:17:32 -0700

    primercomit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bloug::Application.config.secret_token = '2e44698b38f0de6ad352cff7731663911f63b40f9491f96744a6191f6fb57bb98cf20edb5bcb6dcc67db0786e6b6684a657061e2bf27d3a430f2be9a61b928c0'
